5954cc77fe7e8592b3d3bbf6e3b0cac1aa687cf5
Kabirdas Henry <kbrdsk@gmail.com>
2020-09-10 21:27:20 -0700

    Updates

---
+++ precache-manifest.2bd5d78b87b3e362d1dfbe1863243a2a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b400a0f7e98bb11dc731b1f8379158",
+    "url": "/wheres-waldo/index.html"
+  },
+  {
+    "revision": "f9495fe8c0be2f847821",
+    "url": "/wheres-waldo/static/css/main.199b355c.chunk.css"
+  },
+  {
+    "revision": "a49e40e9510680d5746b",
+    "url": "/wheres-waldo/static/js/2.6e1dafe2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wheres-waldo/static/js/2.6e1dafe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9495fe8c0be2f847821",
+    "url": "/wheres-waldo/static/js/main.4d0902a1.chunk.js"
+  },
+  {
+    "revision": "b14359b6740c56bcf460",
+    "url": "/wheres-waldo/static/js/runtime-main.21f80316.js"
+  },
+  {
+    "revision": "a6be3e19bf0a87e0753bc118b25375cc",
+    "url": "/wheres-waldo/static/media/1.a6be3e19.jpg"
+  },
+  {
+    "revision": "3512905a799ff70348a1c95be3527b52",
+    "url": "/wheres-waldo/static/media/2.3512905a.jpg"
+  },
+  {
+    "revision": "5a72110deecd36d592b7fb99af74ee99",
+    "url": "/wheres-waldo/static/media/3.5a72110d.jpg"
+  },
+  {
+    "revision": "6c56a985561bd536de320b2957a8a25f",
+    "url": "/wheres-waldo/static/media/4.6c56a985.jpg"
+  },
+  {
+    "revision": "b489d376bfa901c4be6cd2f7bc46c12d",
+    "url": "/wheres-waldo/static/media/5.b489d376.jpg"
+  },
+  {
+    "revision": "ee0f528281449543808e3d906b2aaa31",
+    "url": "/wheres-waldo/static/media/6.ee0f5282.jpg"
+  },
+  {
+    "revision": "21d007bc0060000e980cfef634772fe1",
+    "url": "/wheres-waldo/static/media/7.21d007bc.jpg"
+  },
+  {
+    "revision": "b80b91b67b139e28b8947aaf51d35c12",
+    "url": "/wheres-waldo/static/media/back.b80b91b6.svg"
+  },
+  {
+    "revision": "00385daf17917b62556a01db120e78cb",
+    "url": "/wheres-waldo/static/media/title.00385daf.jpg"
+  }
+]);
